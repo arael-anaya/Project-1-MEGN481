666979592eed635ba91270208fe566bb4891d8db
Arael Anaya <arael.a.anaya@gmail.com>
2026-02-12 20:26:58 -0700

    fixed d and D choice, as well as removed bad code

--- stage 2/main.py
+++ stage 2/main.py
@@ -22,9 +22,7 @@ class Segment:
         if linked_segment is None:
             return
 
-        D = linked_segment.d
-        d = self.d
-
+        D, d = max(self.d, linked_segment.d), min(self.d, linked_segment.d)
 
         self.Kt = StressConcentration.stress_concentration(D, d, r, "bending")
         self.Kts = StressConcentration.stress_concentration(D, d, r, "torsion")
@@ -35,14 +33,6 @@ class Material:
     name: str
     Sy_psi: float
 
-def worst_case_value(v: Any) -> float:
-    if isinstance(v, (int, float, np.number)):
-        return float(abs(v))
-
-    s = str(v).strip()
-    parts = [p.strip() for p in s.replace(",", "/").split("/")]
-    nums = [abs(float(p)) for p in parts if p != ""]
-    return max(nums) if nums else 0.0
 
 def solve_shaft_iterative(segments, Sy, target_fos, r, tol=1e-4, max_iter=50):
 
@@ -118,6 +108,8 @@ def main():
             print(f"\nTarget FoS = {target_fos}")
             print(f"{material.name} Sy = {Sy} psi\n")
 
+
+            # Iterte through r = .02 to r = .1
             r = 0.06 
 
             segments = solve_shaft_iterative(
@@ -137,14 +129,6 @@ def main():
             # worst_d = 0
             # worst_loc = None
 
-            # for row in table:
-            
-            # DO NOT WORRY ABOUT THIS IT JUST TAKES THE V, M, and T FROM EACH ROW OF THE DICTIONARY
-            # V = worst_case_value(row["V"])
-            # M = worst_case_value(row["M"])
-            # T = worst_case_value(row["T"])
-            # Kt = worst_case_value(row["Kt"])
-            # Kts = worst_case_value(row["Kts"])
 
             # if row["Location"] == "Keyway":
             #     # you need a shaft diameter first
@@ -158,21 +142,6 @@ def main():
             #     print(f"  Required Height = {H_required:.4f} in\n")
                 
             #     continue
-
-            # fos = DiameterCalculations.fos_calculation(V, M, T, Sy, Kt, Kts , d_req)
-
-            # print(f"{row['Location']}")
-            # print(f"  Required diameter = {d_req:.4f} in\n")
-
-
-        # Filler to find the worst fos in the shaft etc etc
-        #     if d_req > worst_d:
-        #         worst_d = d_req
-        #         worst_loc = row["Location"]
-
-        # print(f"Worst-case location: {worst_loc}")
-        # print(f"Governing diameter required: {worst_d:.4f} in\n")
-        # print(f"------------------------------------------------------------------ \n")
 
 if __name__ == "__main__":
     main()
