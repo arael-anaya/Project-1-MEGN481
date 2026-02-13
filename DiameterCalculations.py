22e7bee89d21aa085e734054850742f0ec53d494
Arael Anaya <arael.a.anaya@gmail.com>
2026-02-12 17:34:39 -0700

    added custom kt and kts per section

--- DiameterCalculations.py
+++ DiameterCalculations.py
@@ -12,10 +12,6 @@ from dataclasses import dataclass
 from typing import Dict, Any, List
 import numpy as np
 
-
-
-
-
 @dataclass
 class Material:
     name: str
@@ -41,12 +37,13 @@ def von_mises_stress(d, V, M, T, Kt, Kts):
 
     sigma_b = Kt * (c * M / I)
     tau_t = Kts * (c * T / J)
-    tau_v =0.0
-    # tau_v = 16 * V / (3 * pi * d**2)
+
+    tau_v = 16 * V / (3 * pi * d**2)
 
     tau_combined = tau_t+tau_v
 
-    sigma_vm = np.sqrt(sigma_b**2 + 3 * tau_combined**2)
+    sigma_vm =  np.sqrt(sigma_b**2 + 3 * tau_combined**2)
+
     return sigma_vm
 
 def fos_calculation(V, M ,T, Sy, Kt, Kts ,d):
@@ -79,21 +76,19 @@ def solve_required_diameter(V, M, T, Sy, Kt, Kts, target_fos):
 def main():
 
     table = [
-        {"Location": "Output Spline 1 Shoulder (spline side)", "V": 97, "M": 170, "T": 462.5},
-        {"Location": "Bearing 1 Shoulder (bearing side)", "V": 176, "M": 203, "T": 462.5},
-        {"Location": "Gear Shoulder (gear side)", "V": 176, "M": 1254, "T": 925}, #Jordan said to use 900 here but we should check that
-        {"Location": "Keyway", "V": "176/602", "M": 1363, "T": -462.5},
-        {"Location": "Snap Ring for Gear", "V": 602, "M": 995, "T": -462.5},
-        {"Location": "Bearing 2 Shoulder (bearing side)", "V": 602, "M": 300, "T": -462.5},
-        {"Location": "Input Spline 2 Shoulder (spline side)", "V": 97, "M": 170, "T": -462.5},
+        {"Location": "Output Spline 1 Shoulder (spline side)", "V": 97, "M": 170, "T": 462.5 , "Kt" : 1.0 , "Kts" : 1.0},
+        {"Location": "Bearing 1 Shoulder (bearing side)", "V": 176, "M": 203, "T": 462.5 , "Kt" : 1.0 , "Kts" : 1.0},
+        {"Location": "Gear Shoulder (gear side)", "V": 176, "M": 1254, "T": 925, "Kt" : 1.0 , "Kts" : 1.0}, #Jordan said to use 900 here but we should check that
+        {"Location": "Keyway", "V": "176/602", "M": 1363, "T": -462.5 , "Kt" : 2.3 , "Kts": 3.0 }, #Jordan said to use 
+        {"Location": "Snap Ring for Gear", "V": 602, "M": 995, "T": -462.5 , "Kt" : 3.0 , "Kts" : 5.0},
+        {"Location": "Bearing 2 Shoulder (bearing side)", "V": 602, "M": 300, "T": -462.5 , "Kt" : 1.0 , "Kts" : 1.0},
+        {"Location": "Input Spline 2 Shoulder (spline side)", "V": 97, "M": 170, "T": -462.5 , "Kt" : 1.0 , "Kts" : 1.0},
     ]
     
     target_fos_list = [3.0 , 2.0]   # <-- SET REQUIRED DESIGN FACTOR HERE
 
     materials = [Material("Steel", Sy_psi=60000) , Material("Alluminium" , Sy_psi = 9000)] # < --- SET MATERIAL HERE
 
-    Kt = 2.3
-    Kts = 3.0
 
 
 
@@ -114,6 +109,8 @@ def main():
                 V = worst_case_value(row["V"])
                 M = worst_case_value(row["M"])
                 T = worst_case_value(row["T"])
+                Kt = worst_case_value(row["Kt"])
+                Kts = worst_case_value(row["Kts"])
 
                 #finds the correct diamater for each of the shaft segments
                 d_req = solve_required_diameter(V, M, T, Sy, Kt, Kts , target_fos)
