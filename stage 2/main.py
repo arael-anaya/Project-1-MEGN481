b28d6f2cd4a907df7217ee4fbf2888aafc5d616e
Jordan Rivera <Jordanrivera715278@gmail.com>
2026-02-16 00:54:54 -0700

    Changed torque values (they should all be positive to maximize stress)a

--- stage 2/main.py
+++ stage 2/main.py
@@ -9,7 +9,7 @@ from typing import Dict, Any, List
 import numpy as np
 import copy
 
-MAX_ITER = 20
+MAX_ITER = 25
 TOL = 1e-7
 
 
@@ -126,17 +126,17 @@ def build_segments():
         ),
         "Bearing 2 Shoulder": Segment(
             "Bearing 2 Shoulder",
-            602, 300, -462.5,
+            602, 300, 462.5,
             link="Gear Shoulder"
         ),
         "Input Spline Shoulder": Segment(
             "Input Spline Shoulder",
-            97, 170, -462.5,
+            97, 170, 462.5,
             link="Bearing 2 Shoulder"
         ),
         "Snap Ring" : Segment(
             "Snap Ring" ,
-            602,995,-462.5,
+            602,995,462.5,
             Kt = 3,
             Kts = 5 
             )
